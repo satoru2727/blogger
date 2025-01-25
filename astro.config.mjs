e169f9a1630fb8cc33249cebd9b689434362dc24
satoru2727 <92915718+satoru2727@users.noreply.github.com>
2025-01-25 13:50:28 +0000

    tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,19 +5,22 @@ import mdx from "@astrojs/mdx";
 
 import sitemap from "@astrojs/sitemap";
 
-import tailwind from "@astrojs/tailwind";
 
 import icon from "astro-icon";
 
 import playformCompress from "@playform/compress";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		mdx(),
 		sitemap(),
-		tailwind({ applyBaseStyles: false }),
 		icon(),
 		playformCompress(),
 	],
+	vite:{ 
+	plugins: [tailwindcss()]
+	}
 });
