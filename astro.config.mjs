28c761fbb73c51c75d23ecad39577e22af4caea3
satoru2727 <92915718+satoru2727@users.noreply.github.com>
2025-02-06 11:46:05 +0000

    📝 ✨ (astro.config.mjs, src/pages/blog/[slug].astro, src/pages/philosophy/[slug].astro): Add site property to astro config and improve SEO
    The site property has been added to the `astro.config.mjs` file to improve SEO.  The `post` variable assignment has been moved to improve code readability in the blog and philosophy pages.  This change ensures that the sitemap and other SEO elements are correctly generated.

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,7 @@ import tailwindcss from "@tailwindcss/vite";
 import remarkCallout from "@r4ai/remark-callout";
 import playformInline from "@playform/inline";
 export default defineConfig({
+	site:"https://satoru.work",
     integrations: [mdx(), sitemap(), icon(), playformCompress(), playformInline()],
     vite: {
         plugins: [tailwindcss()],
