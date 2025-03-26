a7189344b681e5aae081765be7b0b325beb533e2
satoru2727 <92915718+satoru2727@users.noreply.github.com>
2025-03-26 19:06:41 +0900

    記事の構成を変更し、ブログと哲学のセクションを削除して、投稿セクションを追加しました。また、関連する型定義を更新しました。

--- src/content.config.ts
+++ src/content.config.ts
@@ -5,8 +5,8 @@ import { defineCollection, z } from "astro:content";
 import { file, glob } from "astro/loaders";
 
 // 3. Define your collection(s)
-const blog = defineCollection({
-	loader: glob({ pattern: "**/*.mdx", base: "./article/blog" }),
+const posts = defineCollection({
+	loader: glob({ pattern: "**/*.mdx", base: "./article/posts" }),
 	schema: z.object({
 		title: z.string(),
 		publishedAt: z.date(),
@@ -14,13 +14,5 @@ const blog = defineCollection({
 		updatedAt: z.date().optional(),
 	}),
 });
-const philosophy = defineCollection({
-	loader: glob({ pattern: "**/*.mdx", base: "./article/philosophy" }),
-	schema: z.object({
-		title: z.string(),
-		publishedAt: z.date(),
-		tag: z.array(z.string()),
-		updatedAt: z.date().optional(),
-	}),
-});
-export const collections = { blog, philosophy };
+
+export const collections = { posts };
